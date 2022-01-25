87555146c4be659ba2ecf068a1fe3007e7b9a3f3
seungyeopme <seungyeop.me.dev@gmail.com>
2022-01-26 08:40:32 +0900

    Swift

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by seungyeop on 2022/01/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var lblHello: UILabel!
+    @IBOutlet var txtName: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnSend(_ sender: UIButton) {
+        lblHello.text = "Hello, " + txtName.text!
+    }
+    
+}
+
