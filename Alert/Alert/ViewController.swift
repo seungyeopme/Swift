1866fcd7a0b9463d41f9530284b663e4b5c07760
seungyeopme <seungyeop.me.dev@gmail.com>
2022-02-09 08:59:36 +0900

    아울렛 변수와 액션 함수 추가하기

--- Alert/Alert/ViewController.swift
+++ Alert/Alert/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lampImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnLampOn(_ sender: UIButton) {
+    }
+    @IBAction func btnLampOff(_ sender: UIButton) {
+    }
+    @IBAction func btnLampRemove(_ sender: UIButton) {
+    }
+    
 }
 
