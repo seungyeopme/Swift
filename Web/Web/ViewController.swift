dd90f0c18cd82fbfffbb9b12887b1fb70a0210a9
seungyeopme <seungyeop.me.dev@gmail.com>
2022-02-15 09:31:20 +0900

    액티비티 인디케이터 뷰에 대한 아울렛 변수 추가

--- Web/Web/ViewController.swift
+++ Web/Web/ViewController.swift
@@ -6,9 +6,14 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var txtUrl: UITextField!
+    @IBOutlet var myWebView: WKWebView!
+    @IBOutlet var myActivityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
