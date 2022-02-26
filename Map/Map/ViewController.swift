22c7af57d5870c8b9c756176b295d487fbda06e4
seungyeopme <seungyeop.me.dev@gmail.com>
2022-02-27 06:15:25 +0900

    아울렛 변수와 액션 함수 추가하기

--- Map/Map/ViewController.swift
+++ Map/Map/ViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var myMap: MKMapView!
+    @IBOutlet var lblLocationInfo1: UILabel!
+    @IBOutlet var lblLocationInfo2: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func sgChangeLocation(_ sender: UISegmentedControl) {
+    }
 }
 
