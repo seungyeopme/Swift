8e7bf2d4d18c673ae8679f0e3e88eee5cd5f9c44
seungyeopme <seungyeop.me.dev@gmail.com>
2022-03-11 09:47:59 +0900

    아울렛 변수와 액션 함수 추가하기

--- PageControl/PageControl/ViewController.swift
+++ PageControl/PageControl/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var pageControl: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func pageChange(_ sender: UIPageControl) {
+        
+    }
+    
 
 }
 
