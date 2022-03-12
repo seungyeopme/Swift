8fcd5dec1241626801ad66e5858632cc054ffc14
seungyeopme <seungyeop.me.dev@gmail.com>
2022-03-12 09:32:01 +0900

    페이지 컨트롤 동작 구현하기

--- PageControl/PageControl/ViewController.swift
+++ PageControl/PageControl/ViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+var images = [ "01.png", "02.png", "03.png", "04.png", "05.png", "06.png" ]
+
 class ViewController: UIViewController {
     @IBOutlet var imgView: UIImageView!
     @IBOutlet var pageControl: UIPageControl!
@@ -14,8 +16,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        pageControl.numberOfPages = images.count
+        
+        pageControl.currentPage = 0
+        
+        pageControl.pageIndicatorTintColor = UIColor.green
+        
+        pageControl.currentPageIndicatorTintColor = UIColor.red
+        
+        imgView.image = UIImage(named: images[0])
     }
+    
     @IBAction func pageChange(_ sender: UIPageControl) {
+        imgView.image = UIImage(named: images[pageControl.currentPage])
         
     }
     
