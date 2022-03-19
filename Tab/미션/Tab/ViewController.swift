241423d9b0ff8ebec20670c086d0025e6a5b0bb8
seungyeopme <seungyeop.me.dev@gmail.com>
2022-03-19 09:19:31 +0900

    새로운 탭 추가하기

---
+++ Tab/미션/Tab/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Tab
+//
+//  Created by BeomGeun Lee on 2020
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnMoveImageView(_ sender: UIButton) {
+        tabBarController?.selectedIndex = 1
+    }
+    
+    @IBAction func btnMoveDatePickerView(_ sender: UIButton) {
+        tabBarController?.selectedIndex = 2
+    }
+}
+
+
+
+
+
