6765a66a86c24527bc18992e817790a60b70f894
seungyeopme <seungyeop.me.dev@gmail.com>
2022-03-24 18:52:36 +0900

    뷰 전환 구분하기

---
+++ Navigation/Navigation/EditViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EditViewController.swift
+//  Navigation
+//
+//  Created by seungyeop on 2022/03/24.
+//
+
+import UIKit
+
+class EditViewController: UIViewController {
+    var textWayValue: String = ""
+    @IBOutlet var lblWay: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        lblWay.text = textWayValue
+    }
+    
+    @IBAction func btnDone(_ sender: UIButton) {
+        _ = navigationController?.popViewController(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Navigation/Navigation/ViewController.swift
+++ Navigation/Navigation/ViewController.swift
@@ -13,7 +13,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let editViewController = segue.destination as! EditViewController
+        if segue.identifier == "editButton" {
+        // 버튼을 클릭한 경우
+            editViewController.textWayValue = "segue :  use button"
+        } else if segue.identifier == "editBarButton" {
+        // 바 버튼을 클릭한 경우
+            editViewController.textWayValue = "segue : use Bar button"
+        }
+    }
 
 }
 
