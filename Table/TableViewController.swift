d4f48b125efcdf766727d2c50c14ba0b1617a798
seungyeopme <seungyeop.me.dev@gmail.com>
2022-04-28 18:25:37 +0900

    목록 삭제하기

--- Table/TableViewController.swift
+++ Table/TableViewController.swift
@@ -55,17 +55,24 @@ class TableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            items.remove(at: (indexPath as NSIndexPath).row)
+            itemsImageFile.remove(at: (indexPath as NSIndexPath).row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
+    
+    override func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexpath: IndexPath) -> String? {
+        return "삭제"
+    }
+    
 
     /*
     // Override to support rearranging the table view.
