3e4b758b0e1608dbdb56727f3a734181ccacbf1b
seungyeopme <seungyeop.me.dev@gmail.com>
2022-04-29 18:40:18 +0900

    바 버튼으로 목록 삭제하기

--- Table/TableViewController.swift
+++ Table/TableViewController.swift
@@ -20,7 +20,7 @@ class TableViewController: UITableViewController {
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+         self.navigationItem.leftBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
