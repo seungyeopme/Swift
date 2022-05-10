86e923328876f47ce310c8af643dc6236f900f2e
seungyeopme <seungyeop.me.dev@gmail.com>
2022-05-10 10:04:09 +0900

    재생 시간 표시하고 볼륨 제어하기

--- Audio/Audio/ViewController.swift
+++ Audio/Audio/ViewController.swift
@@ -11,13 +11,13 @@ import AVFoundation
 class ViewController: UIViewController, AVAudioPlayerDelegate {
     
     var audioPlayer : AVAudioPlayer!
-    
     var audioFile : URL!
     
     let MAX_VOLUME : Float = 10.0
     
     var progressTimer : Timer!
     
+    let timePlayerSelector:Selector = #selector(ViewController.updatePlayTime)
 
     @IBOutlet var pvProgressPlay: UIProgressView!
     @IBOutlet var lblCurrentTime: UILabel!
@@ -76,6 +76,12 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     @IBAction func btnPlayAudio(_ sender: UIButton) {
         audioPlayer.play()
         setPlayButtons(false, pause: true, stop: true)
+        progressTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: timePlayerSelector, userInfo: nil, repeats: true)
+    }
+    
+    @objc func updatePlayTime() {
+        lblCurrentTime.text = convertNSTimeInterval2String(audioPlayer.currentTime)
+        pvProgressPlay.progress = Float(audioPlayer.currentTime/audioPlayer.duration)
     }
     @IBAction func btnPauseAudio(_ sender: UIButton) {
         audioPlayer.pause()
@@ -83,9 +89,17 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     }
     @IBAction func btnStopAudio(_ sender: UIButton) {
         audioPlayer.stop()
+        audioPlayer.currentTime = 0
+        lblCurrentTime.text = convertNSTimeInterval2String(0)
         setPlayButtons(true, pause: false, stop: false)
+        progressTimer.invalidate()
     }
     @IBAction func slChangeVolume(_ sender: UISlider) {
+        audioPlayer.volume = slVolume.value
+    }
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        progressTimer.invalidate()
+        setPlayButtons(true, pause: false, stop: false)
     }
 }
 
