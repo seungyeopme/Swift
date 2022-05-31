cfa623c9c6539f9db8d09fff742e9527f7633d99
seungyeopme <seungyeop.me.dev@gmail.com>
2022-05-31 11:53:21 +0900

    탭과 터치 연습 앱을 위한 기본 환경 구성하기

--- TabTouch/TabTouch/ViewController.swift
+++ TabTouch/TabTouch/ViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var txtMessage: UILabel!
+    @IBOutlet var txtTabCount: UILabel!
+    @IBOutlet var txtTouchCount: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
