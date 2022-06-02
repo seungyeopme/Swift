9f6989d084d0b543a32ef732db22f95bd3907563
seungyeopme <seungyeop.me.dev@gmail.com>
2022-06-02 10:58:25 +0900

    스케치 앱 만들기

---
+++ Sketch/Sketch/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Sketch
+//
+//  Created by seungyeop on 2022/06/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var imgView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func clearImageView(_ sender: UIButton) {
+        
+    }
+    
+}
+
