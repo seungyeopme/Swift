71e7af0f406fb8d0b7bf8a99aee4d3bfd38dfffb
seungyeopme <seungyeop.me.dev@gmail.com>
2022-06-03 20:13:58 +0900

    스케치 앱 기능 구현하기

--- Sketch/Sketch/ViewController.swift
+++ Sketch/Sketch/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var imgView: UIImageView!
     
+    var lastPoint: CGPoint!
+    var lineSize:CGFloat = 2.0
+    var lineColor = UIColor.red.cgColor
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
