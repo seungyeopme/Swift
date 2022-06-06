58577fae0c7707855a4acade707187a781afddde
seungyeopme <seungyeop.me.dev@gmail.com>
2022-06-06 11:40:10 +0900

    아울렛 변수 추가하기

--- SwipeGesture/SwipeGesture/ViewController.swift
+++ SwipeGesture/SwipeGesture/ViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var imgViewUp: UIImageView!
+    @IBOutlet var imgViewDown: UIImageView!
+    @IBOutlet var imgViewLeft: UIImageView!
+    @IBOutlet var imgViewRight: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
