b99269d7f742cb1af56589007510a77140150dca
seungyeopme <seungyeop.me.dev@gmail.com>
2022-06-07 23:44:19 +0900

    한 손가락 스와이프 구현하기

--- SwipeGesture/SwipeGesture/ViewController.swift
+++ SwipeGesture/SwipeGesture/ViewController.swift
@@ -13,9 +13,26 @@ class ViewController: UIViewController {
     @IBOutlet var imgViewLeft: UIImageView!
     @IBOutlet var imgViewRight: UIImageView!
     
+    var imgLeft = [UIImage]()
+    var imgRight = [UIImage]()
+    var imgUp = [UIImage]()
+    var imgDown = [UIImage]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imgUp.append(UIImage(named: "arrow-up-black.png")!)
+        imgUp.append(UIImage(named: "arrow-up-red.png")!)
+        imgDown.append(UIImage(named: "arrow-down-black.png")!)
+        imgDown.append(UIImage(named: "arrow-down-red.png")!)
+        imgLeft.append(UIImage(named: "arrow-left-black.png")!)
+        imgLeft.append(UIImage(named: "arrow-left-red.png")!)
+        imgRight.append(UIImage(named: "arrow-right-black.png")!)
+        imgRight.append(UIImage(named: "arrow-right-red.png")!)
+        
+        
+        
     }
 
 
