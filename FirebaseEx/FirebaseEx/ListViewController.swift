a85a1beffe2dba1eec8ee89f761d81bc1d0bff7e
seungyeopme <seungyeop.me.dev@gmail.com>
2022-07-12 22:45:11 +0900

    ファイアベース-データベース(FirestoreDatabase)-1 5:48

---
+++ FirebaseEx/FirebaseEx/ListViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ListViewController.swift
+//  FirebaseEx
+//
+//  Created by seungyeop on 2022/07/12.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    @IBAction func onBtnAdd(_ sender: UIButton) {
+    }
+    
+    @IBAction func onBtnRead(_ sender: UIButton) {
+    }
+    
+    
+}
