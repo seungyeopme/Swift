f73b6359baa477383fcbabcd2e9aa7b81ab2a70d
seungyeopme <seungyeop.me.dev@gmail.com>
2022-07-14 20:01:39 +0900

    ファイアベース-データベース(FirestoreDatabase)-1 19:13

--- FirebaseEx/FirebaseEx/ListViewController.swift
+++ FirebaseEx/FirebaseEx/ListViewController.swift
@@ -16,7 +16,22 @@ class ListViewController: UIViewController {
     struct IdolData {
         var name: String = ""
         var imageString: String = ""
+        //Firebase DBに入るデータ構造
+        //クラス、構造体が入ることができない
+        //1. Number
+        //2. String
+        //3. Array
+        //4. Dictionary
+        func getDic() -> [String:String] {
+            let dict = ["name": self.name,
+                        "imageString": self.imageString]
+            return dict
+        }
     }
+    
+    //구조체 배열
+    var idolArray: Array<IdolData> = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +42,13 @@ class ListViewController: UIViewController {
     }
     
     @IBAction func onBtnRead(_ sender: UIButton) {
+        readListData()
+    }
+    
+    func readListData() {
+        let db = Firestore.firestore()
+        
+        db.collection("idols").whereField("name", isEqualTo: "Matsumoto Jun")
     }
     
     
