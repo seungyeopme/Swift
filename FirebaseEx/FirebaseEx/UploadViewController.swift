7f4ea85f95c12ef77f0dd4b52ffb39166ed94b36
seungyeopme <seungyeop.me.dev@gmail.com>
2022-07-21 22:08:30 +0900

    ファイアベース-リポジトリ写真アップ(Storage) 10:59

---
+++ FirebaseEx/FirebaseEx/UploadViewController.swift
@@ -0,0 +1,38 @@
+//
+//  UploadViewController.swift
+//  FirebaseEx
+//
+//  Created by seungyeop on 2022/07/21.
+//
+
+import UIKit
+import Firebase
+import FirebaseStorage
+
+class UploadViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+    @IBAction func onBtnUpload(_ sender: UIButton) {
+        uploadImage()
+    }
+    
+    func uploadImage() {
+        let storage = Storage.storage()
+        let storageRef = storage.reference()
+        let data = imageView.image!.pngData()
+        
+        //タイムスタンプ: 1970年1月1日0時0分0秒を基準にミリセカンド単位の時間を表す整数
+    }
+    
+
+}
